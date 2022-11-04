9a38286f02d685a0e5d354b755211d0a9c80fc79
Prashanth <prashanth.subbiah@sjsu.edu>
2022-11-04 16:52:01 -0700

    drone card text changes

--- frontend/src/components/DroneFleetTracking.js
+++ frontend/src/components/DroneFleetTracking.js
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAdminDroneList, capitalizeFirst } from '../utils';
 import { Row, Col, Form, Card, Badge, Spinner } from 'react-bootstrap';
+import { MdOutlineClose } from 'react-icons/md';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 
@@ -11,6 +12,7 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function DroneFleetTracking() {
     const dispatch = useDispatch();
+    const [indDrone, setIndDrone] = useState(null);
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const loading = useSelector((state) => state.admindronetracking.loading);
     const drones = useSelector((state) => state.admindronetracking.data);
@@ -42,6 +44,11 @@ function DroneFleetTracking() {
         zoom: 10
     };
 
+    const selectDrone = (drone) => {
+        console.log(drone.drone_id);
+        setIndDrone(drone)
+    }
+
     return(
         <div className="container">
             <h4>Service cloud Dashboard</h4>
@@ -51,7 +58,7 @@ function DroneFleetTracking() {
                   <span className="visually-hidden">Loading...</span>
                 </Spinner> : ''}
                 {!loading && drones && drones.length && drones.map(drone => 
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: '13rem' }} className={indDrone && (drone.drone_id === indDrone.drone_id) ? 'selected' : ''} onClick={() => selectDrone(drone)}>
                       <Card.Body>
                         <Card.Title>{drone.drone_maker} {drone.drone_model}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Drone #{drone.drone_id}</Card.Subtitle>
@@ -62,7 +69,19 @@ function DroneFleetTracking() {
                 </Card>
                 )}
             </div>
-            <div style={{ height: '670px', width: '100%', marginTop: '10px' }}>
+            {indDrone && indDrone.drone_id ? <div className="drone_details">
+                    <div style={{float: 'right'}}><MdOutlineClose size={40} style={{cursor: 'pointer'}} onClick={() => setIndDrone()} /></div>
+                    <h4>Drone ID #{indDrone.drone_id}</h4>
+                    <Row>
+                        <Col>
+                            <p>Tracking details</p>
+                        </Col>
+                        <Col>
+                            <p>Drone details</p>
+                            <p>Service Type: {indDrone.service_type}</p>
+                        </Col>
+                    </Row>
+                </div> : <div style={{ height: '670px', width: '100%', marginTop: '10px' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: "AIzaSyDAY3yhqc24s5SpDcxRXTnObJXbQTBwtV4" }}
                     defaultCenter={defaultProps.center}
@@ -79,7 +98,7 @@ function DroneFleetTracking() {
                       type={'active'}
                     />
                 </GoogleMapReact>
-            </div>
+            </div>}
         </div>
     )
 }
