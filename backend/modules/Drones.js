5f891ee02a6db9336f1794407696464712fc2552
shravya-k <k.shravya96@gmail.com>
2022-11-06 14:56:06 -0800

    drone database change

--- backend/modules/Drones.js
+++ backend/modules/Drones.js
@@ -1,3 +1,6 @@
+const sequelize = require('sequelize')
+const Op = sequelize.Op
+
 const getDroneDetails = async (req, res, next) => {
     const { models: { drone: Drone } } = COREAPP;
     try {
@@ -5,7 +8,36 @@ const getDroneDetails = async (req, res, next) => {
 		});
         req.model = {};
         req.model.data = droneData;
-        console.log(req.model.data)
+        //console.log(req.model.data)
+        return next();
+    }
+    catch {
+        console.log('error for drones data');
+        return next();
+    }
+};
+const filterDroneDetails = async (req, res, next) => {
+    const { models: { drone: Drone } } = COREAPP;
+    try {
+       
+
+        console.log("@@@@@@@@@@@@@@@@@",req.body)
+        
+		const droneData = await Drone.findAll({
+            //  where: {
+            //     service:req.body.service,
+            //     brand: req.body.brand,
+            //     equipment:req.body.equipment,
+            //     status:req.body.status,
+            //     price:{
+            //         [Op.lt]:req.body.price
+            //     },
+             
+            //  }
+		});
+        req.model = {};
+        req.model.data = droneData;
+        console.log("mode;",req.model.data)
         return next();
     }
     catch {
@@ -14,5 +46,6 @@ const getDroneDetails = async (req, res, next) => {
     }
 };
 module.exports = {
-    getDroneDetails
+    getDroneDetails,
+    filterDroneDetails
 };
