d7093a70f2133a6c7ebfa07372c5dd00dded7cfa
souperala <98585812+souperala@users.noreply.github.com>
2022-11-13 02:44:44 -0800

    Updated Profile Form

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectProfileData } from '../selectors/profileSelector';
 import { FaPencilAlt, FaSave } from 'react-icons/fa';
 import { setToast } from '../actions/app-actions';
-import { Form, Button, Row, Col, Image } from 'react-bootstrap';
+import { Form, Button, Row, Col, Image, FormGroup } from 'react-bootstrap';
 
 export default function Profile() {
 	const dispatch = useDispatch();
@@ -88,101 +88,124 @@ export default function Profile() {
 						<Image src={userProfileForm.dp_url} roundedCircle={true} style={{objectFit: 'cover', width: '200px', height: '200px', marginLeft: 'auto', marginRight: 'auto', display: 'block'}} />
 					</Col>
 					<Col>
-						{editMode ?
-							<>
-							<Form.Label className="form_label" htmlFor="image">User Image</Form.Label>
-							<Form.Control
-							    type="file"
-							    id="image"
-							    aria-describedby="image"
-							    onChange={uploadImage}
-							  />
-							</> : ''}
-						<Form.Label className="form_label" htmlFor="name">Name</Form.Label>
-						{!editMode ? <p>{userProfileForm.name}</p> : 
-							<Form.Control
-							    type="text"
-							    id="name"
-							    aria-describedby="name"
-							    value={userProfileForm.name}
-							    onChange={onUserProfileChange}
-							  />
-						}
-						<Form.Label className="form_label" htmlFor="dob">Date of Birth</Form.Label>
-						{!editMode ? <p>{userProfileForm.dob}</p> : 
-							<Form.Control
-							    type="text"
-							    id="dob"
-							    aria-describedby="name"
-							    value={userProfileForm.dob}
-							    onChange={onUserProfileChange}
-							  />
-						}
-						<Form.Label className="form_label" htmlFor="address1">Address Line 1</Form.Label>
-						{!editMode ? <p>{userProfileForm.address1}</p> : 
-							<Form.Control
-							    type="text"
-							    id="address1"
-							    aria-describedby="address1"
-							    value={userProfileForm.address1}
-							    onChange={onUserProfileChange}
-							  />
-						}
-						<Form.Label className="form_label" htmlFor="name">City</Form.Label>
-						{!editMode ? <p>{userProfileForm.city}</p> : 
-							<Form.Control
-							    type="text"
-							    id="city"
-							    aria-describedby="city"
-							    value={userProfileForm.city}
-							    onChange={onUserProfileChange}
-							  />
-						}
-						<Form.Label className="form_label" htmlFor="state">State</Form.Label>
-						{!editMode ? <p>{userProfileForm.state}</p> : 
-							<Form.Control
-							    type="text"
-							    id="state"
-							    aria-describedby="state"
-							    value={userProfileForm.state}
-							    onChange={onUserProfileChange}
-							  />
-						}
-						<Form.Label className="form_label" htmlFor="country">Country</Form.Label>
-						{!editMode ? <p>{getCountryName(userProfileForm.country)}</p> : 
-							<Form.Select aria-label="Select currency" id="country" value={userProfileForm.country} onChange={onUserProfileChange}>
-		                      <option>Select One</option>
-		                      {countries && countries.map(country => <option value={country.code}>{country.name}</option>)}
-		                    </Form.Select>
-						}
-						<Form.Label className="form_label" htmlFor="email">Email</Form.Label>
-						{!editMode ? <p>{userProfileForm.email}</p> : 
-							<Form.Control
-							    type="text"
-							    id="email"
-							    aria-describedby="email"
-							    value={userProfileForm.email}
-							    onChange={onUserProfileChange}
-							  />
-						}
+						<Form.Group className="UserDetails">
+							{editMode ?
+								<>
+								<Form.Label className="form_label" htmlFor="image">User Image</Form.Label>
+								<Form.Control
+									type="file"
+									id="image"
+									aria-describedby="image"
+									onChange={uploadImage}
+								/>
+								</> : ''}
+						</Form.Group>
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="name">Name</Form.Label>
+							{!editMode ? <p>{userProfileForm.name}</p> : 
+								<Form.Control
+									type="text"
+									id="name"
+									aria-describedby="name"
+									value={userProfileForm.name}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="dob">Date of Birth</Form.Label>
+							{!editMode ? <p>{userProfileForm.dob}</p> : 
+								<Form.Control
+									type="date"
+									id="dob"
+									aria-describedby="name"
+									value={userProfileForm.dob}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>					
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="address1">Address Line 1</Form.Label>
+							{!editMode ? <p>{userProfileForm.address1}</p> : 
+								<Form.Control
+									type="text"
+									id="address1"
+									aria-describedby="address1"
+									value={userProfileForm.address1}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>					
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="name">City</Form.Label>
+							{!editMode ? <p>{userProfileForm.city}</p> : 
+								<Form.Control
+									type="text"
+									id="city"
+									aria-describedby="city"
+									value={userProfileForm.city}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>						
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="state">State</Form.Label>
+							{!editMode ? <p>{userProfileForm.state}</p> : 
+								<Form.Control
+									type="text"
+									id="state"
+									aria-describedby="state"
+									value={userProfileForm.state}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>
+						<Form.Group className="UserDetails">
+							<Form.Label htmlFor="country" className="form_label">Country</Form.Label>
+							{!editMode ? <p>{getCountryName(userProfileForm.country)}</p> : 
+								<Form.Select className="form_label" id="country" value={userProfileForm.country} onChange={onUserProfileChange}>
+								<option>Choose</option>
+								<option value="India">India</option>
+								<option value="United States">United States</option>
+								<option value="United Kingdom">United Kingdom</option>
+								<option value="South Africa">South Africa</option>
+								{countries && countries.map(country => <option value={country.code}>{country.name}</option>)}
+								</Form.Select>
+							}
+						</Form.Group>
+						<Form.Group className="UserDetails">
+							<Form.Label className="form_label" htmlFor="email">Email</Form.Label>
+							{!editMode ? <p>{userProfileForm.email}</p> : 
+								<Form.Control
+									type="text"
+									id="email"
+									aria-describedby="email"
+									value={userProfileForm.email}
+									onChange={onUserProfileChange}
+								/>
+							}
+						</Form.Group>
+						<Form.Group className="UserDetails">
 						<Form.Label className="form_label" htmlFor="dp_url">Picture URL</Form.Label>
-						{!editMode ? <p>{userProfileForm.dp_url}</p> : 
-							<Form.Control
-							    type="text"
-							    id="dp_url"
-							    aria-describedby="dp_url"
-							    disabled={true}
-							    value={userProfileForm.dp_url}
-							  />
-						}
-						{editMode ? <div className="btn_panel">
-							<Button variant="secondary" onClick={() => reset()}>
-		                        Reset
-		                    </Button>
-		                    <Button variant="primary" onClick={() => submitProfile()}>
-		                        Save Changes
-		                    </Button>
-						</div> : ''}
+							{!editMode ? <p>{userProfileForm.dp_url}</p> : 
+								<Form.Control
+									type="text"
+									id="dp_url"
+									aria-describedby="dp_url"
+									disabled={true}
+									value={userProfileForm.dp_url}
+								/>
+							}
+							{editMode ? <div className="btn_panel">
+								<Button variant="secondary" onClick={() => reset()}>
+									Reset
+								</Button>
+								<Button variant="primary" onClick={() => submitProfile()}>
+									Save Changes
+								</Button>
+							</div> : ''}
+						</Form.Group>
+					
 					</Col>
 				</Row>
 			</div>
