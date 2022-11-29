aff286bc2fe1dfb4476179c86d06cf2ec742d399
shravya-k <k.shravya96@gmail.com>
2022-11-29 10:40:28 -0800

    scheduler and booking changes

--- backend/modules/Booking.js
+++ backend/modules/Booking.js
@@ -97,9 +97,11 @@ const getUserBookings = async(req, res, next) => {
 }
 const BookingDroneScheduler = async(req, res, next) => {
     const { models: { booking: Booking } } = COREAPP;
+
     try {
         var currDate = moment().toDate();
         var newDateObj = moment().add(5, 'minutes').toDate();
+        console.log("time now",currDate,"time in 5 mins",newDateObj)
 
         const userbookings = await Booking.findAll({
             where:{
@@ -109,15 +111,12 @@ const BookingDroneScheduler = async(req, res, next) => {
                     [Op.lte]: newDateObj
                 }
             },
+          
             raw: true
         });
-        console.log(userbookings)
-        console.log(currDate,newDateObj)
-        // const x = {
-        //     droneid:"1",
-        //     serviceid:"22",
-        //     plan:{"fileType":"Plan","mission":{"cruiseSpeed":15,"hoverSpeed":5,"items":[{"altitude":50.00000000005293,"latitude":37.558605,"longitude":-122.04754200000004,"type":"SimpleItem"},{"altitude":49.99999999958835,"latitude":37.55864599999999,"longitude":-122.04716700000002,"type":"SimpleItem"},{"altitude":50.00000000023102,"latitude":37.55854299999999,"longitude":-122.04714700000001,"type":"SimpleItem"},{"altitude":50.00000000013322,"latitude":37.55850099999999,"longitude":-122.04753699999999,"type":"SimpleItem"},{"altitude":49.99999999947457,"latitude":37.55857599999999,"longitude":-122.04759100000001,"type":"SimpleItem"},{"altitude":14.99999999988875,"latitude":37.55835499999999,"longitude":-122.047656,"type":"SimpleItem"},{"altitude":14.99999999990697,"latitude":37.558277999999994,"longitude":-122.04764200000001,"type":"SimpleItem"},{"altitude":14.999999999058375,"latitude":37.55827200000002,"longitude":-122.04751300000001,"type":"SimpleItem"},{"altitude":14.999999999587114,"latitude":37.558371999999984,"longitude":-122.047544,"type":"SimpleItem"},{"altitude":14.999999999485869,"latitude":37.558385999999985,"longitude":-122.047313,"type":"SimpleItem"},{"altitude":15.00000000058955,"latitude":37.558317,"longitude":-122.04730100000003,"type":"SimpleItem"},{"altitude":14.99999999915223,"latitude":37.558319,"longitude":-122.04721900000001,"type":"SimpleItem"},{"altitude":14.999999998778456,"latitude":37.5584,"longitude":-122.047232,"type":"SimpleItem"}]}}
-        // }
+        // console.log("boooookings",userbookings)
+        // console.log(currDate,newDateObj)
+ 
         
         return userbookings
         
